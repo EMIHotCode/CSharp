2e301b532f170e67cd116f70164c13beef96b6d6
EMIHotCode <terminal7@dtn.ru>
2024-08-26 17:14:36 +0300

    some chang

--- HomeWork20_08_24/Program.cs
+++ HomeWork20_08_24/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+
 
 namespace Programm
 {
@@ -16,15 +15,34 @@ namespace Programm
             Console.WriteLine("\t\tМеню");
             Console.WriteLine("1. Создать текстовый файл в текущей директории программы");
             Console.WriteLine("2. Записать форматированную информацию в файл");
-            Console.WriteLine("3. Открыть и прочесть файл");
-            Console.WriteLine("4. ВЫХОД");
+            Console.WriteLine("3. Открыть и прочесть файл созданый в пункте 1 Меню");
+            Console.WriteLine("4. Преобразовать данные из файла в переменные с учетом структуры");
+            Console.WriteLine("5. ВЫХОД");
         }
         static void Main(string[] args)
         {
 
             bool exit = false;
             int choise;
-            string fileName, filePath = ".\\";
+
+            string fileName, filePath = ".\\", FIO;
+            DateTime myBirthday = new DateTime(1984, 12, 3);
+
+            double[,] massiv =
+            {
+                { 1.23, 2.25, 3.3},
+                { 1.1324, 2.23, 3.78},
+                { 4.1, 5.2, 3},
+                { 4.05, 5.012210, 3.05}
+            };
+
+            int[,] massivInt =
+            {
+                { 1, 2, 3, 4},
+                { 5, 6, 7, 8},
+                { 9, 10, 11, 12}
+            };
+
 
             while (!exit)
             {
@@ -74,20 +92,6 @@ namespace Programm
 
                     case 2:
                         string str = string.Empty;
-                        double[,] massiv = 
-                        {
-                            { 1.23, 2.25, 3.3},
-                            { 1.1324, 2.23, 3.78},
-                            { 4.1, 5.2, 3},
-                            { 4.05, 5.012210, 3.05}
-                        };
-
-                        int[,] massivInt =
-                        {
-                            { 1, 2, 3, 4},
-                            { 5, 6, 7, 8},
-                            { 9, 10, 11, 12}
-                        };
 
                         if (filePath == ".\\")
                         {
@@ -99,8 +103,7 @@ namespace Programm
                             using (StreamWriter writer = new StreamWriter(fileStream, Encoding.Unicode)) 
                             {
                                 Console.WriteLine("\nЗапись в файл ФИО и DateTime{дата рождения} -> Ефремов Михаил Иванович 12.03.1984");
-                                DateTime myBirthday = new DateTime(1984, 12, 3);
-                                string FIO = "Ефремов Михаил Иванович";
+                                FIO = "Ефремов Михаил Иванович";
                                 str = FIO + " " + myBirthday.Month.ToString() + " " + myBirthday.Day.ToString() + " " + myBirthday.Year.ToString();
                                 writer.WriteLine(str);
                                 Thread.Sleep(1000);
@@ -163,7 +166,6 @@ namespace Programm
                             }
                         }
                         break;
-
                     case 3:
                         if (filePath != ".\\")
                         {
@@ -172,9 +174,85 @@ namespace Programm
                             {
                                 using (StreamReader reader = new StreamReader(filePath, Encoding.Unicode))
                                 {
-                                    Console.WriteLine(reader.ReadToEnd()); // чтение из файла
+                                    str = reader.ReadToEnd(); // чтение из файла
+
+                                    Console.WriteLine($"{str}\n\nФайл успешно прочитан.");
                                 }
-                                Console.WriteLine("\nФайл успешно прочитан.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("\nТакого файла в текущем каталоге нет. Укажите другое имя файла пункт Меню 1");
+                            }
+                        }
+                        else
+                            Console.WriteLine("\nОШИБКА. Укажите имя файла пункт Меню 1 который будем выводить на экран");
+                        break;
+                    case 4:
+
+                        if (filePath != ".\\")
+                        {
+                            FileInfo fileInf = new FileInfo(filePath);
+                            if (fileInf.Exists)
+                            {
+                                using (StreamReader reader = new StreamReader(filePath, Encoding.Unicode))
+                                {
+                                    // восстановление из 1 строки
+                                    str = reader.ReadLine(); // чтение из файла
+                                    string[] textArr = str.Split(" ,.:\t*\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                                    FIO = textArr[0] + " " + textArr[1] + " " + textArr[2]; // Возвращаем имя фамилию и отчество в переменную FIO
+                                    DateTime testBirthday = new DateTime(Convert.ToInt32(textArr[5]), Convert.ToInt32(textArr[3]), Convert.ToInt32(textArr[4]));
+                                    Console.WriteLine($"\nФамилие Имя Отчество восстановлено в переменную =  {FIO}\n" +
+                                        $"Дата рождения восстановлена из файла в DateTime = {testBirthday.Month} {testBirthday.Day} {testBirthday.Year}");
+
+                                    // восстановление из 2 строки
+                                    str = reader.ReadLine(); // чтение из файла
+                                    textArr = str.Split(" ,.:\t*\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                                    massiv = new double[Convert.ToInt32(textArr[0]), Convert.ToInt32(textArr[1])];
+                                    Console.WriteLine($"\nЧисло строк {Convert.ToInt32(textArr[0])} и стоблцов {Convert.ToInt32(textArr[1])}  массива дробных чисел восстановлено.");
+
+                                    // восстановление из 3, 4, 5, 6 строки
+                                    Console.WriteLine($"\nВывод массива дробных чисел восстановленный из файла:");
+
+                                    for (int i = 0; i < massiv.GetLength(0); i++)
+                                    {
+                                        str = reader.ReadLine();
+                                        textArr = str.Split(" \n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                                        for (int j = 0; j < massiv.GetLength(1); j++)
+                                        {
+                                            massiv[i, j] = Convert.ToDouble(textArr[j]);
+                                            Console.Write($"{massiv[i, j]} ");
+                                        }
+                                        Console.WriteLine();
+                                    }
+
+                                    // восстановление строк массива целых чисел 7, 8
+                                    str = reader.ReadLine(); // чтение из файла
+                                    textArr = str.Split(" ,.:\t*\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                                    massivInt = new int[Convert.ToInt32(textArr[0]), Convert.ToInt32(textArr[1])];
+                                    Console.WriteLine($"\nЧисло строк {Convert.ToInt32(textArr[0])} и стоблцов {Convert.ToInt32(textArr[1])}  массива целых чисел восстановлено.");
+
+                                    // восстановление массива целых чисел 
+                                    Console.WriteLine($"\nВывод массива целых чисел восстановленный из файла:");
+                                    str = reader.ReadLine();
+                                    textArr = str.Split(" \n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                                    for (int i = 0; i < massivInt.GetLength(0); i++)
+                                    {
+                                        for (int j = 0, k = (i * 4); j < 4; j++, k++)
+                                        {
+                                            massivInt[i, j] = Convert.ToInt32(textArr[k]);
+                                            Console.Write($"{massivInt[i, j]} ");
+                                        }
+                                        Console.WriteLine();
+                                    }
+
+                                    // восстановление из 1 строки
+                                    str = reader.ReadLine(); // чтение из файла
+                                    textArr = str.Split(" ,.:\t*\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                                    DateTime dateFromFile = new DateTime(Convert.ToInt32(textArr[2]), Convert.ToInt32(textArr[1]), Convert.ToInt32(textArr[0]));
+                                    Console.WriteLine($"\nДата восстановлена из файла в DateTime Год - {dateFromFile.Year} Месяц - {dateFromFile.Month} День - {dateFromFile.Day} ");
+
+                                }
+
                             }
                             else
                             {
@@ -186,7 +264,7 @@ namespace Programm
 
                     break;
 
-                    case 4:
+                    case 5:
                         return;
 
                     default:
